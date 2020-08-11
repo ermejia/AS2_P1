dd44bb4426a808a72ed368f6c6d2219b6037a51a
ronald <ottoniel.21@outlook.es>
2020-08-10 20:28:16 -0600

    Creación del proyecto SpringBoot con las dependencias solicitadas

---
+++ src/gt/edu/umg/consultaEstudianteCurso/ConsultaEstudianteCursoApplication.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.consultaEstudianteCurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaEstudianteCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaEstudianteCursoApplication.class, args);
+	}
+
+}
