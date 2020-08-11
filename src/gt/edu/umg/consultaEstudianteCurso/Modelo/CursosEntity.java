3d9a27a75d172627870c5e04cf29483227c8084c
oscarrl98 <oscar_rlp@hotmail.es>
2020-08-10 22:45:21 -0600

    Se han creado el modelo CursosEntity

---
+++ src/gt/edu/umg/consultaEstudianteCurso/Modelo/CursosEntity.java
@@ -0,0 +1,31 @@
+package gt.edu.umg.consultaEstudianteCurso.Modelo;
+
+public class CursosEntity {
+    private int idCurso;
+    private String name;
+    private String descripcion;
+
+    public int getIdCurso() {
+        return idCurso;
+    }
+
+    public void setIdCurso(int idCurso) {
+        this.idCurso = idCurso;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
