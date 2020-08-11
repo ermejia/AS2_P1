92020dff6666086677afb46be44b7e1896a95ac8
ermejia <mejiagalveze@outlook.com>
2020-08-10 22:46:31 -0600

    Clase de la Conexion hacia MySql

---
+++ src/gt/edu/umg/consultaEstudianteCurso/Controller/Conexion.java
@@ -0,0 +1,37 @@
+package gt.edu.umg.consultaEstudianteCurso.Controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import java.util.*;
+
+public class Conexion{
+
+    private Connection conexion = null;
+    private Statement stm = null;
+
+    public Conexion() {
+    }
+
+    public Connection conectar() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexion = DriverManager.getConnection("jdbc:mysql://192.185.4.65:3306/jbarilla_ingsoftware", "jbarilla_estudia", "2Ui!OssHDQGv");
+        } catch (Exception e) {
+        }
+        return conexion;
+    }
+
+    public void desconectar() {
+
+        try {
+            conexion.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+}
