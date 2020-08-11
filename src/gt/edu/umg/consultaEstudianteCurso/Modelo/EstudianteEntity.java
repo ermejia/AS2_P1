4ca505207c2e46645b0d7f00522a8dfffb3737b3
ronald <ottoniel.21@outlook.es>
2020-08-10 22:31:35 -0600

    Creacion de los atributos en la clase EstudiantesEntity

---
+++ src/gt/edu/umg/consultaEstudianteCurso/Modelo/EstudianteEntity.java
@@ -0,0 +1,95 @@
+package gt.edu.umg.consultaEstudianteCurso.Modelo;
+
+public class EstudianteEntity {
+    private String id;
+    private String name;
+    private String surname;
+    private String email;
+    private String birthdate;
+    private String studentId;
+    private String phone1;
+    private String phone2;
+    private String address1;
+    private String address2;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getPhone1() {
+        return phone1;
+    }
+
+    public void setPhone1(String phone1) {
+        this.phone1 = phone1;
+    }
+
+    public String getPhone2() {
+        return phone2;
+    }
+
+    public void setPhone2(String phone2) {
+        this.phone2 = phone2;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+}
